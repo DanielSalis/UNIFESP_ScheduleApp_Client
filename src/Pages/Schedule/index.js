719d70c87e96586cef2a33fb5e60405e21575761
DanielSalis <danielsalis98@gmail.com>
2021-01-20 16:53:28 -0300

    feat: adiconando header na página de Schedule

--- src/Pages/Schedule/index.js
+++ src/Pages/Schedule/index.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import { PageContainer, Body } from "./style";
+
+import Header from '../../Components/Header';
 class Schedule extends Component{
     
     render(){
         return(
             <PageContainer>
+                <Header />
                 <Body></Body>
             </PageContainer>
         )
